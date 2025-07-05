deb43de167372f30dc6827095d6a2fdaaffa6e0c
nazar_netrebko <netrebkon@gmail.com>
2025-07-05 12:55:31 +0300

    .env fixes

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -2,6 +2,7 @@ import { ContactsCollection } from '../db/models/contacts.js';
 
 export const getAllContacts = async () => {
   const contacts = await ContactsCollection.find();
+  console.log('Contacts found:', contacts);
   return contacts;
 };
 
